2b25e8f46a68dbf79bc9bb7fa7e9b56d6be85019
talladac <figuracionc@gmail.com>
2024-09-23 11:31:20 +0400

    Finished core requirements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?: ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+        string letter = "";
+    
+
+        if ( number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80 )
+        {
+            letter = " B ";
+        }
+        else if (number >= 70 )
+        {
+            letter = " C";
+        }
+        else if ( number >= 60 )
+        {
+            letter = "D";
+        }
+        else 
+
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine ($"Your grade level is {letter} ");
+
+        if ( number >= 70 )
+        {
+            Console.WriteLine ("Congratulations! You passed!!!");
+        }
+        else{
+            Console.WriteLine ("Try again!! You can do it!");
+        }
     }
+
 }
